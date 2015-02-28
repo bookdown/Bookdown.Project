63acb9c12dd5c4e349c690f101fb81cf9607f718
Paul M. Jones <pmjones88@gmail.com>
2015-02-28 09:47:55 -0600

    major reorg

--- _bookdown/templates/head.php
+++ _bookdown/templates/head.php
@@ -1,5 +1,4 @@
 <?php use Aura\Html\Escaper as e; ?>
 <head>
     <title><?= e::h($this->page->getTitle()); ?></title>
-    <link rel="stylesheet" type="text/css" href="/style.css" />
 </head>
--- bin/bookdown.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(E_ALL);
-require dirname(__DIR__) . '/vendor/autoload.php';
-$container = new Bookdown\Bookdown\Container();
-$command = $container->newCommand($GLOBALS);
-exit($command());
