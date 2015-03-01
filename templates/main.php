3d3c973c3550141bb86fa41681d77616b2507d92
Paul M. Jones <pmjones88@gmail.com>
2015-03-01 16:13:13 -0600

    update template control

---
+++ templates/main.php
@@ -0,0 +1,20 @@
+<?php
+// default library templates
+$library = dirname(__DIR__) . "/vendor/bookdown/bookdown/templates";
+
+// project-specific templates
+$project = __DIR__;
+
+// register the templates
+$templates = $this->getViewRegistry();
+$templates->set("head", "{$project}/head.php");
+$templates->set("body", "{$library}/body.php");
+$templates->set("core", "{$library}/core.php");
+$templates->set("navheader", "{$library}/navheader.php");
+$templates->set("navfooter", "{$library}/navfooter.php");
+$templates->set("toc", "{$library}/toc.php");
+?>
+<html>
+<?php echo $this->render("head"); ?>
+<?php echo $this->render("body"); ?>
+</html>
